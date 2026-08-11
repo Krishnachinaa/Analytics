425cb34c999e3aaeb2b60e0972767518209e954a
sundarivenkatasai@gmail.com <sundarivenkatasai@gmail.com>
2026-08-11 12:44:47 +0000

    s

--- models/dim_customers_incremental.sql
+++ models/dim_customers_incremental.sql
@@ -9,7 +9,7 @@ from {{ ref('stg_customers') }}
 
 {% if is_incremental() %}
 where created_date > (
-    select max(created_date)
+    select max(stg_customers.created_date)
     from {{ this }}
 )
 {% endif %}
